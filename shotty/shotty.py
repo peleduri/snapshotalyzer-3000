8efcb375a79225e0df86464651b31e78ae56755b
urip <urip@jfrog.com>
2018-12-30 01:46:57 +0200

    List volumes and snapshots. Naive implementation of snapshots command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -19,13 +19,79 @@ def filter_instances(project):
 def cli():
     """shotty manages snapshots"""
 
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+    
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+
+                )))
+
+    return
+
 @cli.group('volumes')
 def volumes():
     """Commands for volumes"""
 
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "Gib",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+    return
+
 @cli.group('instances')
 def instances():
     """Commands for instances"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Creae snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Desciption="Created by SnapshotAlyzer 3000")
+
+    return
+
 
 @instances.command('list')
 @click.option('--project', default=None,
