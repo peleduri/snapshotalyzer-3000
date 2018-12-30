a9fd69648ec944f843affd61692db495c4dce162
urip <urip@jfrog.com>
2018-12-30 02:11:37 +0200

    Wait for instances to stop and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -85,10 +85,21 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+     
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
-            v.create_snapshot(Desciption="Created by SnapshotAlyzer 3000")
+            print(" Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
+        
+        print("Starting {0}...".format(i.id))
+        
+        i.start()
+        i.wait_until_running()
+        
+    print("Job's DONE!")
 
     return
 
