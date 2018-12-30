e755bd4fb87bf3c67e0bcc490e5e8cdf8a1328f4
urip <urip@jfrog.com>
2018-12-30 02:45:10 +0200

    snapshots already in progress lets not start a new snapshot #3

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """shotty manages snapshots"""
@@ -96,6 +100,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(" Skipping{0}. snapshots already in progress".format(v.id))
+                continue
+
             print(" Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
         
